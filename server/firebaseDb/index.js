4d1daa799741582f7f715a890e36cf02d83c84ef
Hack Reactor Student <student@hackreactor.com>
2018-01-04 19:25:11 -0800

    Lay out for exporting db functions complete

--- server/firebaseDb/index.js
+++ server/firebaseDb/index.js
@@ -11,3 +11,63 @@ firebase.initializeApp(CONFIG);
 //firebase database
 const database = firebase.database();
 
+//exporting functions
+
+createUser((username, first_name, last_name, user_ID) => {
+
+});
+
+createPhoto ((photo_URL, user_ID, caption) => {
+   // returns generated photo_ID
+});
+
+getUserID ((username) => {
+   // returns generated user_ID
+});
+
+addPhotoTags((photo_ID, tag_ID) => {
+
+});
+
+getTagFromName((first_name) => {
+  //returns tag_ID
+});
+
+getTagFromPhoto((photo_ID)=> {
+  //returns tag_ID
+});
+
+getNameFromTag((tag_ID)=> {
+  //returns full_name
+});
+
+getAllFaceIDs(()=> {
+  //returns [face_ID] in an array?
+});
+
+getAllPhotos(() => {
+  //returns [{photoURL, caption, likes, tags, faceRectangle}]
+});
+
+increaseLike((photoURL) => {
+
+});
+
+decreaseLike((photoURL) => {
+
+});
+
+getLike((photoURL) => {
+//returns likes from DB 
+});
+createTag((face_ID) => {
+
+});
+
+addUserToTag((face_ID, userID) => {
+
+});
+
+addNameToTag((face_ID, first_name, last_name) => {
+
+});
