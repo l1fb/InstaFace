38bff9e6ac4610618223d64ea2f32f652b7c5b8c
Diego Quintanal <dquintanalccb@gmail.com>
2018-01-06 14:54:16 -0800

    [Face Recognition Functions] Successfull communication with API, finalizing function

--- server/facerecofuncs/detect.js
+++ server/facerecofuncs/detect.js
@@ -1,21 +1,54 @@
 const parser = require('body-parser');
 const axios = require('axios');
+const request = require('request');
 
 const detectFace = function (imageUrl) {
     var returnObj = {
         faceDetected: false,
         faceRectangle: {
-            top: 0,
-            left: 0,
+            topLeftX: 0,
+            topLeftY: 0,
             width: 0,
-            height: 0
         }
     };
+
+    imageUrl = imageUrl;
+    bodybod = {
+        "image": imageUrl,
+        "selector": "ROLL" 
+    }
+
+
+    function processImage() {
+        request({
+            method: 'POST',
+            url: 'https://api.kairos.com/detect',
+            headers: {
+              'Content-Type': 'application/json',
+              'app_id': '562b6c3e',
+              'app_key': 'c10f1cb231d8b685c3a376dee49e75f6'
+            },
+            body: JSON.stringify(bodybod),
+          }, function (error, response, body) {
+            // console.log('Status:', response.statusCode);
+            // console.log('Headers:', JSON.stringify(response.headers));
+            // console.log('Response:', body);
+            returnObj.faceRectangle.topLeftX = response.images[0].faces.topLeftX;
+            returnObj.faceRectangle.topLeftY = response.images.faces.topLeftY;
+            returnObj.faceRectangle.width = response.images.faces.width;
+
+
+          });
+    };
+
+    processImage();
 
 
     return returnObj
 }
 
+
+module.exports.detectFace = detectFace;
 
 // DETECTFACE DOCUMENTATION
 // ========================   
@@ -30,9 +63,11 @@ const detectFace = function (imageUrl) {
 // Return value: Object containing:
 //     1) faceDetected: true if face is detected
 //     2) faceRectangle: object conatining coordinates of face 
-//    rectangle (top, left, width and height)
+//        rectangle (top, left, width and height)
+//    3) FaceId: faceId as provided by API
 // 
 // Notes:
-
-//     - Face Rectangle is a box that Azure's face regonition API 
-//     creates around a face it recognizes. 
+//     - Face Rectangle is a box that Kairo's face regonition API 
+//     creates around a face it recognizes. One additional thing to
+//     note is that the returned coordinates all begin at the top left 
+//     of the photo.
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const express = require('express')
 const app = express(); 
 const routes = require('./routes'); 
 const morgan = require('morgan')
+const face = require('./facerecofuncs/detect.js');
 
 app.use(parser.json()); 
 app.use(parser.urlencoded({extended: true})); 
@@ -19,3 +20,7 @@ app.use(express.static(path.resolve(__dirname, '../client/public')));
 app.listen(PORT, () => {
     console.log("app is listening on port ", PORT); 
 })
+
+console.log(face.detectFace("http://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/m(01-32)_gr.jpg"));
+
+
