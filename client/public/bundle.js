729fcd0cb798e5dd0e244d1a315f1dd6c2473bc7
Shivani Patel <shivanidp7@gmail.com>
2018-01-11 15:57:40 -0800

    "merge conflicts"

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -28362,7 +28362,7 @@ exports.default = Header;
 /***/ (function(module, exports) {
 
 "use strict";
-throw new Error("Module build failed: SyntaxError: Unexpected token (9:6)\n\n\u001b[0m \u001b[90m  7 | \u001b[39m  \u001b[36mclass\u001b[39m \u001b[33mAuthorization\u001b[39m \u001b[36mextends\u001b[39m \u001b[33mComponent\u001b[39m {\n \u001b[90m  8 | \u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  9 | \u001b[39m\u001b[36mclass\u001b[39m \u001b[33mAuthorization\u001b[39m \u001b[36mextends\u001b[39m \u001b[33mComponent\u001b[39m {\n \u001b[90m    | \u001b[39m      \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 10 | \u001b[39m  constructor(props) {\n \u001b[90m 11 | \u001b[39m    \u001b[36msuper\u001b[39m(props)\u001b[33m;\u001b[39m \n \u001b[90m 12 | \u001b[39m    fire\u001b[33m.\u001b[39minitFirebase(()\u001b[33m=>\u001b[39m{})\u001b[33m;\u001b[39m\u001b[0m\n");
+throw new Error("Module build failed: SyntaxError: Unexpected token (6:0)\n\n\u001b[0m \u001b[90m 4 | \u001b[39m\u001b[36mimport\u001b[39m fire from \u001b[32m'../firebaseAuth'\u001b[39m\u001b[33m;\u001b[39m\n \u001b[90m 5 | \u001b[39m\u001b[36mimport\u001b[39m setUser from \u001b[32m'../reducers/setUser'\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 6 | \u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<\u001b[39m \u001b[33mHEAD\u001b[39m\n \u001b[90m   | \u001b[39m\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 7 | \u001b[39m  \n \u001b[90m 8 | \u001b[39m\u001b[33m||\u001b[39m\u001b[33m||\u001b[39m\u001b[33m||\u001b[39m\u001b[33m|\u001b[39m merged common ancestors\n \u001b[90m 9 | \u001b[39m\u001b[33m===\u001b[39m\u001b[33m===\u001b[39m\u001b[33m=\u001b[39m\u001b[0m\n");
 
 /***/ }),
 /* 448 */
