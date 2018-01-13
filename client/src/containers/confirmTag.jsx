73cf2ee6d454603728ce346d157ba65ea096854e
Sylvain Ung <sylvainung@live.com>
2018-01-12 18:32:42 -0800

    Sort feed by like and clear form after submit

--- client/src/containers/confirmTag.jsx
+++ client/src/containers/confirmTag.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class ConfirmTag extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tagConfirmation: '',
+      captionInput: ''
+    };
+  }
+
+  onChangeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         ConfirmTag says hi
+        - display photo that was just uploaded
+        - create an input field for tag confirmation
+        - create an input field for caption 
       </div>
     );
   }
