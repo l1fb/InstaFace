8edb986ba0e5ecb078d4b88cb47a19a937b6fa96
Sylvain Ung <sylvainung@live.com>
2018-01-14 13:58:06 -0800

    Fixed photoController error

--- server/routes/Controllers/PhotoController.js
+++ server/routes/Controllers/PhotoController.js
@@ -3,10 +3,8 @@ const detectFace = require('../../facerecofuncs/detect');
 const enrollFace = require('../../facerecofuncs/enroll');
 const recognizeFace = require('../../facerecofuncs/recognize'); 
 const hostImage = require('../../imagehosting/hosting');
-const axios = require('axios');
-
+const axios = require('axios'); 
 const PhotoController = {
-
     getPhoto: ((req, res) => {
         let url = req.originalUrl.slice(1)
         axios.get('http://'+ url, {responseType: 'arraybuffer'})
@@ -18,7 +16,6 @@ const PhotoController = {
             console.log('error', error)
         }); 
     }),
-
     createPhoto : ((req, res) => {
         let photo = req.file.path;
         let photo_URL;
@@ -35,47 +32,31 @@ const PhotoController = {
                 else {
                     returnObj.name = "Anonomyous"
                 }
-
-                // console.log(returnObj);
                 res.status(201).send(returnObj); 
             });
         })
-        recognizeFace.recognizeFace(photo_URL, (result) => {
-            let returnObj = {faceRectangle : result.faceRectangle}             
-            if (result.candidates && result.candidates[0].confidence > 0.50) {
-                returnObj.name = result.candidates[0].subject_id; 
-            }
-            else {
-                returnObj.name = "Anonomyous"
-            }
-            res.send(returnObj); 
-        }); 
     }),
-
     getAllPhotos : ((req, res) => {
         firebaseDatabase.getAllPhotos(function(allPhotos) {
             res.status(200).send(allPhotos);
           });
     }), 
-
     increaseLike : ((req, res) => {
         firebaseDatabase.increaseLike(req.body.photo_ID);
         res.status(201).send("Increased a Like! spread the love! <3");
     }), 
-
     decreaseLike : ((req, res) => {
         firebaseDatabase.decreaseLike(req.body.photo_URL);
         // console.log("decreaseLike routes responding!", req);
         res.status(201).send("Decresased a Like :( why tho..");
     }),
-
     getPhotoInfo : ((req, res) => {
-        firebaseDatabase.getPhotoInfo(req.headers.query, function(photoInfo) {
+        firebaseDatabase.getPhotoInfo(req.query.photo_URL, function(photoInfo) {
             res.status(200).send(photoInfo);
           })
     }),
-
     addPhotoTags : ((req, res) => {
+        let faceRectangle = req.body.faceRectangle;
         let caption = req.body.caption; 
         let photo_URL = req.body.photo_URL; 
         let photo_ID = photo_URL.split('/')[1]; 
@@ -88,27 +69,24 @@ const PhotoController = {
         firebaseDatabase.createPhoto(photo_ID, photo_URL, user_ID);
         enrollFace.enrollFace('http://' + photo_URL, req.body.tag_name, (bool) => {
         if (bool) {
-                firebaseDatabase.addPhotoTags(photo_ID, req.body.tag_name, req.body.face_Rectangle);
-                res.status(202).send('successfully added a tag on the photo');
+                firebaseDatabase.addPhotoTags(photo_ID, req.body.tag_name, faceRectangle);
+                res.status(201).send('successfully added a tag on the photo');
             }
             else {
                 res.status(500).send('could not add tag on the photo')
             }
         })
     }), 
-
     getPhotoByTag : ((req, res) => {
         firebaseDatabase.getPhotoByTag(req.query.tag_name, function(photos) {
             res.status(200).send(photos);
           });
     }), 
-
     addCaption : ((req, res) => {
         let photo_ID = req.body.photo_URL.split('/')[1]; 
         firebaseDatabase.addCaption(photo_ID, req.body.caption)
         res.status(201).send();
     }),
-
     getPhotoByUserID : ((req, res) => {
         console.log('this is headers', req.headers)
         firebaseDatabase.getPhotoByUserID(req.query.user_ID, function(photos) {
@@ -119,7 +97,5 @@ const PhotoController = {
             res.status(200).send(photos);  
         });
     })
-
 }
-
 module.exports = PhotoController; 
